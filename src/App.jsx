3242ccc37a0caeb3408532d5601a783be6232475
Nikhil Kole <nikhilkole.html@gmail.com>
2025-04-27 14:12:29 +0530

    Uploadimage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ImagePreviewer from './components/ImagePreviewer'
 import ImageUploader from "./components/ImageUploader";
+
 const App = () => {
   return (
-    <div className='flex  text-center justify-center items-center flex-col p-10 w-screen relative'>
-      <h1 className='text-xl uppercase font-extrabold'>AI-Powered Image Enhancer</h1>
-      <p className='mb-10 font-light text-black/70'>Enhance your images effortlessly with our advanced AI technology. It's free and no login is required. Simply upload your image to get started.</p>
-      <ImagePreviewer/>
+    <div className='min-h-screen flex font-sans text-center justify-center items-center flex-col p-10 w-screen relative bg-black text-white bg-[linear-gradient(rgba(255,255,255,0.1)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.1)_1px,transparent_1px)] bg-[size:25px_25px]'>
+      <h1 className='text-2xl uppercase font-extrabold'>AI-Powered Image Enhancer</h1>
+      <p className='mb-10 text-xs font-medium text-white/70'>Enhance your images effortlessly with our advanced AI technology. It's free and no login is required. Simply upload your image to get started.</p>
       <ImageUploader/>
+      <ImagePreviewer/>
     </div>
   )
 }
--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const ImageUploader = () => {
   return (
-    <div className='h-[40vh] w-[90vw] border-2 border-gray-700 relative p-2'>
-      <label htmlFor="fileUpload" className='w-full h-full border-2 border-dashed border-gray-700/70 text-xl text-black hover:border-blue-500 active:text-black/50 flex items-center justify-center'>
+    <div className='h-[40vh] w-[90vw] border-2 border-gray-400 relative p-2 bg-black/30 rounded-lg'>
+      <label htmlFor="fileUpload" className='w-full h-full font-bold transition-all duration-200 border-2 border-dashed border-gray-400/70 text-xl text-white hover:border-blue-400 hover:text-blue-400 active:text-white/50 flex flex-col items-center justify-center gap-4'>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+        </svg>
         Click to upload an image
       </label>
       <input id="fileUpload" type="file" accept='image/*' className='hidden'/>
